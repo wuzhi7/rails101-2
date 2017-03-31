c417ac14e0cab2acc40fe4d218d82f30c08d2ab1
liyangbin <liyanbin98@gmail.com>
2017-03-31 21:57:46 +0800

    implement my groups

--- app/controllers/account/groups_controller.rb
+++ app/controllers/account/groups_controller.rb
@@ -2,7 +2,7 @@ class Account::GroupsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @group = current_user.participated_groups
+    @groups = current_user.participated_groups
   end
-  
+
 end
